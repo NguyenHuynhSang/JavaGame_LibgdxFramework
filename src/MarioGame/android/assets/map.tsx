1b48409cc6ee42cf3bd5cfc0e7fe192e5cce6969
Bùi Đang Quy <37075303+mu4eve@users.noreply.github.com>
2019-05-19 14:28:32 +0700

    fix export apk, clean up code
    add mario head shape to check collision.(only check collision when mario is jumping this mean the mario's head collision with coin, bricks...stuff)

--- src/MarioGame/android/assets/map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="map" tilewidth="32" tileheight="32" tilecount="742" columns="106">
- <image source="level1-1.png" width="3392" height="224"/>
-</tileset>
